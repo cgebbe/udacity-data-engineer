3797e4aa3fba11e980e93cd89b2ce0b0c0876beb
Christian Gebbe <>
2023-08-03 00:02:48 +0200

    feat: staging operator works

--- 4_pipelines/project/template/dags/udac_example_dag.py
+++ 4_pipelines/project/template/dags/udac_example_dag.py
@@ -5,11 +5,12 @@ from airflow.decorators import dag
 
 from airflow.operators.postgres_operator import PostgresOperator
 
-import operators
-import helpers
-
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+try:
+    from ..plugins import operators
+    from ..plugins import helpers
+except:
+    import operators
+    import helpers
 
 
 import pendulum
@@ -20,25 +21,37 @@ from pathlib import Path
 def _filter_empty_items(lst):
     return [x for x in lst if x]
 
+
 def _create_tables_op():
     drop_queries = []
-    for t in ["artists","songplays", "songs","staging_events","staging_songs","time","users"]:
+    for t in [
+        "artists",
+        "songplays",
+        "songs",
+        "staging_events",
+        "staging_songs",
+        "time",
+        "users",
+    ]:
         drop_queries.append(f" DROP TABLE IF EXISTS {t};")
-    
+
     filepath = Path(__file__).parent / "new/create_tables.sql"
     assert filepath.exists(), filepath
-    create_queries = [s.replace("\n","").replace("\t","") for s in filepath.read_text().split(";")]
-    
+    create_queries = [
+        s.replace("\n", "").replace("\t", "") for s in filepath.read_text().split(";")
+    ]
+
     all_queries = _filter_empty_items(drop_queries + create_queries)
 
     return PostgresOperator(
-            task_id="create_tables",
-            sql=all_queries,
-            postgres_conn_id="redshift",
-            autocommit=True,
-        )
+        task_id="create_tables",
+        sql=all_queries,
+        postgres_conn_id="redshift",
+        autocommit=True,
+    )
 
-@dag(  
+
+@dag(
     "udacity",
     default_args={
         "owner": "udacity",
@@ -54,7 +67,6 @@ def _create_tables_op():
     description="Load and transform data in Redshift with Airflow",
     # is scheduled every hour. If set, does not activate directly.
     # schedule_interval="0 * * * *",
-        
 )
 def pipe():
     start_operator = DummyOperator(task_id="Begin_execution")
@@ -63,21 +75,27 @@ def pipe():
     create_tables = _create_tables_op()
     start_operator >> create_tables
 
+    # STAGE
+    stage_songs_to_redshift = operators.StageToRedshiftOperator(
+        task_id="stage_songs",
+        redshift_table_name="staging_songs",
+        s3_path="s3://udacity-dataengineer-pipeline-project-s3/song_data/A/A/A/",
+        json_method="auto",
+    )
+    stage_events_to_redshift = operators.StageToRedshiftOperator(
+        task_id="stage_events",
+        redshift_table_name="staging_events",
+        s3_path="s3://udacity-dataengineer-pipeline-project-s3/log_data",
+        json_method="s3://udacity-dataengineer-pipeline-project-s3/log_json_path.json",
+    )
+    create_tables >> stage_events_to_redshift
+    create_tables >> stage_songs_to_redshift
+
 
 pipe_dag = pipe()
 
-# STAGE
 
 if 0:
-    stage_events_to_redshift = operators.StageToRedshiftOperator(
-        task_id="Stage_events", dag=dag
-    )
-    stage_songs_to_redshift = operators.StageToRedshiftOperator(
-        task_id="Stage_songs", dag=dag
-    )
-
-    start_operator << stage_events_to_redshift
-    start_operator << stage_songs_to_redshift
 
     # LOAD SONGPLAYS
 
@@ -88,7 +106,7 @@ if 0:
     stage_events_to_redshift << load_songplays_table
     stage_songs_to_redshift << load_songplays_table
 
-    # LOAD DIM TABLES 
+    # LOAD DIM TABLES
 
     load_user_dimension_table = operators.LoadDimensionOperator(
         task_id="Load_user_dim_table", dag=dag
--- 4_pipelines/project/template/plugins/operators/stage_redshift.py
+++ 4_pipelines/project/template/plugins/operators/stage_redshift.py
@@ -1,32 +1,49 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import inspect
+
+
+def _clean_query(q: str):
+    return inspect.cleandoc(q).replace("\n ", " ")
+
 
 class StageToRedshiftOperator(BaseOperator):
-    ui_color = '#358140'
+    ui_color = "#358140"
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 s3_path,
-                 redshift_table_name,
-                 *args,
-                 **kwargs,
-                 ):
-
+    def __init__(
+        self,
+        # Define your operators params (with defaults) here
+        # Example:
+        # redshift_conn_id=your-connection-name
+        s3_path: str,
+        redshift_table_name: str,
+        json_method: str,
+        *args,
+        **kwargs,
+    ):
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+
         self.s3_path = s3_path
         self.redshift_table_name = redshift_table_name
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.json_method = json_method
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        del context
 
+        query = f"""
+        COPY {self.redshift_table_name}
+        FROM '{self.s3_path}'
+        IAM_ROLE 'arn:aws:iam::561130499334:role/my-redshift-service-role'
+        JSON '{self.json_method}'
+        STATUPDATE ON
+        MAXERROR 1
+        COMPUPDATE OFF;
+        """
 
-
-
-
+        hook = PostgresHook(postgres_conn_id="redshift")
+        hook.run(
+            sql=_clean_query(query),
+            autocommit=True,
+        )
